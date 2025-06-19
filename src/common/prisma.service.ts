e3a0ead56435abc86a6ba14a03ef13cdb1611f0d
4212ganteng <azizmuslim051@gmail.com>
2025-06-19 14:18:02 +0700

    setup logger prisma

---
+++ src/common/prisma.service.ts
@@ -0,0 +1,34 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { PrismaClient, Prisma } from '@prisma/client';
+import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
+import { Logger } from 'winston';
+@Injectable()
+export class PrismaService extends PrismaClient<
+  Prisma.PrismaClientOptions,
+  string
+> {
+  constructor(
+    @Inject(WINSTON_MODULE_PROVIDER) private readonly logger: Logger,
+  ) {
+    super({
+      log: [
+        {
+          emit: 'event',
+          level: 'info',
+        },
+        {
+          emit: 'event',
+          level: 'warn',
+        },
+        {
+          emit: 'event',
+          level: 'error',
+        },
+        {
+          emit: 'event',
+          level: 'query',
+        },
+      ],
+    });
+  }
+}
